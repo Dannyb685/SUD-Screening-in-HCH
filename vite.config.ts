4073f54075ba04aedaa991b222b2e59a1e135471
Dannyb685 <60555926+Dannyb685@users.noreply.github.com>
2025-12-17 19:57:05 -0800

    feat: Update dependencies and Vite config for single file build
    Updates dependencies for React, Three.js, and related libraries to their latest versions.
    Introduces `vite-plugin-singlefile` and configures Vite for an inline build, ensuring all assets are embedded within a single HTML file. This simplifies deployment and reduces external dependencies.

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,21 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-  },
+  plugins: [react(), viteSingleFile()],
   build: {
+    target: "esnext",
+    assetsInlineLimit: 100000000,
+    chunkSizeWarningLimit: 100000000,
+    cssCodeSplit: false,
     outDir: 'dist',
-    sourcemap: true,
-  }
+    rollupOptions: {
+      inlineDynamicImports: true,
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 });
